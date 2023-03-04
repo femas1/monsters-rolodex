7e089ce4e29d1c8e31b570549a45955a2f7c8531
femas1 <federico.massara2@gmail.com>
2023-03-04 13:02:10 +0100

    add search input component

--- src/App.js
+++ src/App.js
@@ -10,29 +10,53 @@ class App extends Component {
   this.state = {
     monsters: []
   }
+  console.log('constructor')
  }
 
 componentDidMount() {
+  console.log('componentDidMount')
   fetch('https://jsonplaceholder.typicode.com/users')
     .then(res => res.json())
-    .then(users => this.setState(() => {
+    .then(users => this.setState(
+      () => {
       return {monsters: users}
     },
-    () => {
-      console.log(this.state)
-    }))
+      () => {
+        console.log(this.state);
+      }))
 }
 
   render() {
+    console.log('render')
     return (
       <div className="App">
+        <input 
+            className='search-box' 
+            type='search' 
+            placeholder='Search monsters' 
+            onChange={
+              (e) => {
+                // console.log(e.target.value)
+                const searchString = e.target.value.toLocaleLowerCase();
+                const filteredMonsters = this.state.monsters.filter((monster) => {
+                  return monster.name.toLocaleLowerCase().includes(searchString);
+                });
+
+                this.setState(() => {
+                  return {monsters: filteredMonsters};
+                })
+              }
+        }/>
         {this.state.monsters.map((monster) => {
-          return <div key={monster.id}>
-              <h1>{monster.name}</h1>
-            </div>
+          return (
+              <div key={monster.id}>
+                <h1>{monster.name}</h1>
+              </div>
+            );
         })}
       </div>
     );
+
   }
 }
 

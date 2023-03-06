fc5ca2834569a77db8d6e8a62fb366763c648564
femas1 <federico.massara2@gmail.com>
2023-03-06 19:15:43 +0100

    add cardlist component rendering!

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Component } from 'react';
 
-
+import CardList from './components/card-list/card-list.component';
 import './App.css';
 
 class App extends Component {
@@ -11,20 +11,15 @@ class App extends Component {
     monsters: [],
     searchField: ''
   };
-  console.log('constructor')
  }
 
 componentDidMount() {
-  console.log('componentDidMount')
   fetch('https://jsonplaceholder.typicode.com/users')
     .then(res => res.json())
     .then(users => this.setState(
       () => {
       return {monsters: users}
-    },
-      () => {
-        console.log(this.state);
-      }))
+    }))
 }
 
   onSearchChange = (e) => {
@@ -35,7 +30,6 @@ componentDidMount() {
   }
 
   render() {
-    console.log('render')
     const { monsters, searchField } = this.state;
     const { onSearchChange } = this;
     const filteredMonsters = monsters.filter((monster) => {
@@ -49,13 +43,7 @@ componentDidMount() {
             type='search' 
             placeholder='Search monsters' 
             onChange={onSearchChange}/>
-        {filteredMonsters.map((monster) => {
-          return (
-              <div key={monster.id}>
-                <h1>{monster.name}</h1>
-              </div>
-            );
-        })}
+        <CardList monsters={filteredMonsters}/>
       </div>
     );
 
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,11 +1,26 @@
 import { Component } from 'react';
 
 class CardList extends Component {
-    render() {
-        return(
-            <div> Hello I'm the CardList Component</div>
+    render(){
+        const { monsters } = this.props;
+
+        return (
+        <div>
+                {monsters.map((monster) => (
+                  <h1 key={monster.id}>{monster.name}</h1>
+                ))}
+        </div>
+            
         )
     }
 }
 
 export default CardList;
+
+// {filteredMonsters.map((monster) => {
+//     return (
+//         <div key={monster.id}>
+//           <h1>{monster.name}</h1>
+//         </div>
+//       );
+//   })}
